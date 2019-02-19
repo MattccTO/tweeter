a1187c19d744e9ed5dbd1ef70978927876541a33
Matt Cook <mcc1985@gmail.com>
2019-02-19 22:26:39 +0000

    Add tweets container html structure and initial css formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
   $('textarea').on('input', function () {
     const charCount = this.value.length;
-    const count = $(this).parent().find('.counter');
-    count.text(function () {
+    const $count = $(this).parent().find('.counter');
+    $count.text(function () {
       return 140 - charCount;
     });
     if (charCount > 140) {
-      count.addClass('negative');
+      $count.addClass('negative');
     } else {
-      count.removeClass('negative');
+      $count.removeClass('negative');
     }
   });
 });
