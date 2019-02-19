ca2827ae970db5d2d3f082b977c0709184536e84
Matt Cook <mcc1985@gmail.com>
2019-02-19 19:48:34 +0000

    Add a functional character counter based on string length in the text box
    Format counter based on +/-

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('textarea').on('input', function () {
+    const charCount = this.value.length;
+    const count = $(this).parent().find('.counter');
+    count.text(function () {
+      return 140 - charCount;
+    });
+    if (charCount > 140) {
+      count.addClass('negative');
+    } else {
+      count.removeClass('negative');
+    }
+  });
+});
