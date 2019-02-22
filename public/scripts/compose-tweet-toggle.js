354118a3f59dbc47ac4af716ab01ba9cfd7e683c
Matt Cook <mcc1985@gmail.com>
2019-02-22 18:25:13 +0000

    Error msg now correctly hides when compose tweet box collapses

--- public/scripts/compose-tweet-toggle.js
+++ public/scripts/compose-tweet-toggle.js
@@ -3,6 +3,9 @@ $(document).ready(() => {
   $('.composeButton').click(function () {
     $('.new-tweet').slideToggle(400, function () {
       $('textarea').focus();
+      if ($('.errorMsg').is(':Visible')) {
+        $('.errorMsg').slideUp(50);
+      }
     });
   });
 });
